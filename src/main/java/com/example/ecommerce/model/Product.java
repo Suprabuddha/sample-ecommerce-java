d43416ae254c29b168aab2eff1b71e806a36e5ae
Suprabuddha Nag <nagsupra@amazon.com>
2026-01-07 11:37:10 +0530

    Step 5: Complete Jakarta EE Migration - Update All Import Statements Build status: Success

--- src/main/java/com/example/ecommerce/model/Product.java
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -2,8 +2,8 @@ package com.example.ecommerce.model;
 
 import org.apache.commons.lang.StringUtils;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Date;
 
