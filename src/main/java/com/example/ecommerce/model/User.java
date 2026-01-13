45f1d6e70af2c4d51ea844e2b9a5bb3ba22daee9
Suprabuddha <suprabuddha.nag@gmail.com>
2026-01-13 08:41:22 +0000

    Step 4: Complete Jakarta EE Migration - Dependencies and Imports Build status: Success

--- src/main/java/com/example/ecommerce/model/User.java
+++ src/main/java/com/example/ecommerce/model/User.java
@@ -2,8 +2,8 @@ package com.example.ecommerce.model;
 
 import org.apache.commons.lang.StringUtils;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
